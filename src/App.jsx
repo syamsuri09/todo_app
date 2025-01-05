ebf304c3782c58ccbc314dc6f86f05110c4307e2
syamsuri09 <syamsuri270901@gmail.com>
2025-01-05 14:57:40 +0530

    updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import './App.css'
+import { MdDeleteForever } from "react-icons/md";
 
 const App = () => {
 
@@ -9,18 +10,15 @@ const App = () => {
 
   const handleSubmit = () => {
     const inputValue = inputRef.current.value;
-  // Check if the input value is empty
   if (inputValue === "") {
     alert('Please enter a value');
   } else {
     setData([...data, inputValue]);
   }
-
-    inputRef.current.value = '';  // Clear the input field
+  inputRef.current.value = '';  
 };
 
 const handleRemoveItem = (indexToRemove) => {
-    // Remove item at the specified index
     const updatedData = data.filter((item, index) => index !== indexToRemove);
     console.log(indexToRemove)
     setData(updatedData);
@@ -31,15 +29,23 @@ const handleRemoveItem = (indexToRemove) => {
       <div className='todo-container'>
         <h1>Todo List</h1>
       <div className='input-container'>
-     <input ref={inputRef} type="text" className='todo-input' placeholder='Add Your Task' />
+     <input ref={inputRef} type="text" className='todo-input' placeholder='Add Your Task' onKeyDown={(e) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  }} />
       <div type='submit' className='todo-add-btn' onClick={handleSubmit}>Submit</div>
       </div>
-      <ul>
+      <ul className='todo-list'>
       {data.map((item, index) => (
-          <li key={index} onClick={() => handleRemoveItem(index)}>
-            {item}
-          </li>
+        <li>
+          <li className="todo-list-items"key={index} >
+      {item}
+      <MdDeleteForever className="clear-button"  onClick={() => handleRemoveItem(index)} />
+    </li>
+    </li>
         ))}
+        
       </ul>
       </div>
       
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This exposes the server to the network
+    port: 3000,  // You can specify a different port if you want
+  },
 })
